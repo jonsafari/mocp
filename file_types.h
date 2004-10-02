7114aa3d653dfeb60ecab0b8dca6548e070ae307
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-10-02 17:56:26 +0000

    New struct decoder_funcs instead of just play/info functions.
    git-svn-id: svn://svn.daper.net/moc/trunk@253 910807d9-36e0-0310-a014-e9ea483e2ba4

--- file_types.h
+++ file_types.h
@@ -2,13 +2,32 @@
 #define FILE_TYPES_H
 
 #include "playlist.h"
-#include "buf.h"
 
-typedef void (*play_func_t)(const char *, struct buf *out_buf);
-typedef void (*info_func_t)(const char *, struct file_tags *);
+/* Functions that must be provided to support a file format.
+ * void *data is a pointer to private data used by the decoder passed to each
+ * function. */
+struct decoder_funcs
+{
+	/* Open a file and return data used by the decoder. Return NULL on
+	 * error*/
+	void *(*open)(const char *file);
 
-info_func_t get_info_func (char *file);
-play_func_t get_play_func (char *file);
+	/* Close the file and cleanup. */
+	void (*close)(void *data);
+
+	/* Decode a piece of input and write it to the buf of size buf_len.
+	 * Return the number of butes written or 0 on EOF. */
+	int (*decode)(void *data, char *buf, int buf_len);
+
+	/* Seek in the stream by n seconds. */
+	void (*seek)(void *data, const int n);
+
+	/* Fill the tags structure for a file. */
+	void (*info)(const char *file, struct file_tags *tags);
+};
+
 int is_sound_file (char *name);
+struct decoder_funcs *get_decoder_funcs (char *file);
+void file_types_init ();
 
 #endif
