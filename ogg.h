370fb7b62380e3d3384718e91b893487daac1c18
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-10-03 10:44:47 +0000

    Updated prototypes.
    git-svn-id: svn://svn.daper.net/moc/trunk@281 910807d9-36e0-0310-a014-e9ea483e2ba4

--- ogg.h
+++ ogg.h
@@ -1,9 +1,8 @@
 #ifndef OGG_H
 #define OGG_H
 
-#include "buf.h"
+#include "file_types.h"
 
-void ogg_play (const char *file, struct buf *out_buf);
-void ogg_info (const char *file_name, struct file_tags *info);
+struct decoder_funcs *ogg_get_funcs ();
 
 #endif
