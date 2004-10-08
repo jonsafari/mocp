d5971c6fc7a18e23df59399f7941a595f35d6d86
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-10-08 19:35:48 +0000

    Added printf __attribute__ to interface_fatal().
    git-svn-id: svn://svn.daper.net/moc/trunk@365 910807d9-36e0-0310-a014-e9ea483e2ba4

--- interface.h
+++ interface.h
@@ -6,6 +6,12 @@ void init_interface (const int sock, const int debug, char **args,
 void interface_loop ();
 int server_connect ();
 void interface_end ();
+
+#ifdef HAVE__ATTRIBUTE__
+void interface_error (const char *format, ...)
+	__attribute__((format (printf, 1, 2)));
+#else
 void interface_error (const char *format, ...);
+#endif
 
 #endif
