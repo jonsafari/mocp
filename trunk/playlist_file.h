780b8afdb6742451298b4df98d5ef0d9965a4a67
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-10-12 20:03:22 +0000

    Updated prototypes.

--- trunk/playlist_file.h
+++ trunk/playlist_file.h
@@ -2,6 +2,7 @@
 #define PLAYLIST_FILE_H
 
 int plist_load (struct plist *plist, const char *fname, const char *cwd);
+int plist_save (struct plist *plist, const char *file, const char *cwd);
 int is_plist_file (char *name);
 
 #endif
