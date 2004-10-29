346bc4c29fb6d42414426c3b3f14eaf34e77cf86
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-10-29 17:46:26 +0000

    Added format __attribute__ to error().

--- trunk/server.h
+++ trunk/server.h
@@ -3,7 +3,11 @@
 
 int server_init (int debug, int foreground);
 void server_loop (int list_sock);
+#ifdef HAVE_ATTRIBUTE__
+void error (const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+#else
 void error (const char *format, ...);
+#endif
 void state_change ();
 void set_info_rate (const int rate);
 void set_info_channels (const int channels);
