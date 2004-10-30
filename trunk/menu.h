c09d244137b1e0c51e8131149f0d93242ea36af0
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-10-30 11:53:19 +0000

    Added time and format fields to the item structure that are optionaly shown at the right side of files.

--- trunk/menu.h
+++ trunk/menu.h
@@ -17,13 +17,22 @@ enum menu_request
 struct menu_item
 {
 	char *title;		/* Titile of the item */
-	int attr_normal;	/* Attributes in normal mode */
-	int attr_sel;		/* Attributes in selected mode */
-	int attr_marked;	/* Attributes when marked. */
-	int attr_sel_marked;	/* Attributes in selected and marked mode */
-	int plist_pos;		/* Position of that item on the playlist */
-	char *file;		/* File associated with the item. */ 
-	enum file_type type;	/* Type of the associated file. */
+
+	/* Curses attributes in different states: */
+	int attr_normal;
+	int attr_sel;
+	int attr_marked;
+	int attr_sel_marked;
+	
+	int plist_pos;		/* Position of the item on the playlist */
+
+	/* Associated file: */
+	char *file;
+	enum file_type type;
+
+	/* Additional information shown: */
+	char time[6];		/* File time string */
+	char format[4];		/* File format */
 };
 
 struct menu
@@ -36,15 +45,27 @@ struct menu
 	int maxy; /* maximum y position */
 	int selected; /* selected item */
 	int marked; /* index of the marked item or -1 */
+
+	/* Flags for displaying information about the file. */
+	int show_time;
+	int show_format;
+
+	int info_attr; /* Attributes for information about the file */
 };
 
 struct menu *menu_new (WINDOW *win, struct menu_item **items, const int nitems);
+
 struct menu_item *menu_newitem (char *title, const int plist_pos,
 		const enum file_type type, const char *file);
 void menu_item_set_attr_normal (struct menu_item *item, const int attr);
 void menu_item_set_attr_sel (struct menu_item *item, const int attr);
 void menu_item_set_attr_sel_marked (struct menu_item *item, const int attr);
 void menu_item_set_attr_marked (struct menu_item *item, const int attr);
+void menu_item_set_time_plist (struct menu *menu, const int plist_num,
+		const char *time);
+void menu_item_set_time (struct menu_item *item, const char *time);
+void menu_item_set_format (struct menu_item *item, const char *format);
+
 void menu_free (struct menu *menu);
 void menu_driver (struct menu *menu, enum menu_request req);
 struct menu_item *menu_curritem (struct menu *menu);
@@ -60,5 +81,8 @@ void menu_set_top_item (struct menu *menu, const int num);
 int menu_find_pattern_next (struct menu *menu, const char *pattern,
 		const int current);
 int menu_next_turn (const struct menu *menu);
+void menu_set_show_time (struct menu *menu, const int t);
+void menu_set_show_format (struct menu *menu, const int t);
+void menu_set_info_attr (struct menu *menu, const int attr);
 
 #endif
