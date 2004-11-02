d013feffbd58cfaf30145f99c2747c4c44d81222
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-11-02 10:48:07 +0000

    Updated prototypes.

--- trunk/player.h
+++ trunk/player.h
@@ -5,9 +5,10 @@
 #include "buf.h"
 
 void player_cleanup ();
-void player (const char *file, struct decoder_funcs *f, struct buf *out_buf);
+void player (char *file, char *next_file, struct buf *out_buf);
 void player_stop ();
 void player_seek (const int n);
 void player_reset ();
+void player_init ();
 
 #endif
