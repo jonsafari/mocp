42a2d87f481dac76e2f7660467fe75e2d3e95b04
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2004-12-24 21:59:54 +0000

    Replaced interface_error() with general error routine.

--- trunk/interface.h
+++ trunk/interface.h
@@ -7,12 +7,6 @@ void interface_loop ();
 int server_connect ();
 void interface_end ();
 int user_wants_interrupt ();
-
-#ifdef HAVE__ATTRIBUTE__
-void interface_error (const char *format, ...)
-	__attribute__((format (printf, 1, 2)));
-#else
-void interface_error (const char *format, ...);
-#endif
+void interface_error (const char *msg);
 
 #endif
