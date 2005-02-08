ab9e813451b378e8faad2430489920db695abce6
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2005-02-08 19:52:55 +0000

    mark_file() never gets NULL file argument.
    git-svn-id: svn://svn.daper.net/moc/trunk@961 910807d9-36e0-0310-a014-e9ea483e2ba4

--- interface.c
+++ interface.c
@@ -1138,33 +1138,23 @@ static void send_playlist (struct plist *plist, const int clear)
 	}
 }
 
-/* Mark this file from the playlist with this serial number in the menu.
- * Umark if file is NULL. */
+/* Mark this file from the playlist with this serial number in the menu. */
 static void mark_file (const char *file, const int plist_serial)
 {
-	if (file) {
-		int i;
-		
-		if (curr_plist_menu
-				&& plist_serial == plist_get_serial(curr_plist)
-				&& (i = plist_find_fname(curr_plist, file))
-				!= -1)
-			menu_mark_plist_item (curr_plist_menu, i);
+	int i;
 
-		if (playlist_menu && plist_serial == plist_get_serial(playlist)
-				&& (i = plist_find_fname(playlist, file))
-				!= -1)
-			menu_mark_plist_item (playlist_menu, i);
-		
-		if (main_win_mode == WIN_MENU)
-			menu_draw (curr_menu);
-	}
-	else {
-		if (curr_plist_menu)
-			menu_unmark_item (curr_plist_menu);
-		if (playlist_menu)
-			menu_unmark_item (playlist_menu);
-	}
+	assert (file != NULL);
+	
+	if (curr_plist_menu && plist_serial == plist_get_serial(curr_plist)
+			&& (i = plist_find_fname(curr_plist, file)) != -1)
+		menu_mark_plist_item (curr_plist_menu, i);
+
+	if (playlist_menu && plist_serial == plist_get_serial(playlist)
+			&& (i = plist_find_fname(playlist, file)) != -1)
+		menu_mark_plist_item (playlist_menu, i);
+	
+	if (main_win_mode == WIN_MENU)
+		menu_draw (curr_menu);
 }
 
 /* Update the xterm title. */
