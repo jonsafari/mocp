911a65ebe0dec72abbd0648baf890713346197e0
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2005-04-17 19:21:32 +0000

    Updated copyright year.

--- trunk/decoder_plugins/flac/flac.c
+++ trunk/decoder_plugins/flac/flac.c
@@ -1,6 +1,6 @@
 /*
  * MOC - music on console
- * Copyright (C) 2004 Damian Pietras <daper@daper.net>
+ * Copyright (C) 2005 Damian Pietras <daper@daper.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
