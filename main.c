1fe5af48b22389b2f0446a6fc6d3c13bd7e1dc22
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2005-05-18 17:16:22 +0000

    Fixed xrealloc() bahaviour when the size parameter is 0.
    git-svn-id: svn://svn.daper.net/moc/trunk@1316 910807d9-36e0-0310-a014-e9ea483e2ba4

--- main.c
+++ main.c
@@ -105,7 +105,7 @@ void *xrealloc (void *ptr, const size_t size)
 {
 	void *p;
 
-	if ((p = realloc(ptr, size)) == NULL)
+	if ((p = realloc(ptr, size)) == NULL && size != 0)
 		fatal ("Can't allocate memory!");
 	return p;
 
