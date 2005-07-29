dd602bce188cc0397c6f1f3bcda91b48faa8f1ff
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2005-07-29 08:50:24 +0000

    Fixed the syntax (not a proper C code). Taken from the FreeBSD port.
    git-svn-id: svn://svn.daper.net/moc/trunk@1495 910807d9-36e0-0310-a014-e9ea483e2ba4

--- decoder_plugins/flac/flac.c
+++ decoder_plugins/flac/flac.c
@@ -422,12 +422,13 @@ static void flac_info (const char *file_name, struct file_tags *info,
 static int flac_seek (void *void_data, int sec)
 {
 	struct flac_data *data = (struct flac_data *)void_data;
+	FLAC__uint64 target_sample;
 
 	if (sec < 0 || (unsigned)sec > data->length)
 		return -1;
 
-	FLAC__uint64 target_sample = (FLAC__uint64)((sec/(double)data->length)
-			* (double)data->total_samples);
+	target_sample = (FLAC__uint64)((sec/(double)data->length) *
+			(double)data->total_samples);
 	
 	if (FLAC__seekable_stream_decoder_seek_absolute(data->decoder,
 				target_sample))
