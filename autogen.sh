907a9d895e0d3944688d4a17761eed06e07252d2
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2007-02-03 19:26:58 +0000

    Added information about required packages displayed when autogen.sh fails.
    git-svn-id: svn://svn.daper.net/moc/trunk@1965 910807d9-36e0-0310-a014-e9ea483e2ba4

--- autogen.sh
+++ autogen.sh
@@ -11,3 +11,13 @@ autoheader && \
 automake -W all -a && \
 autoconf -W syntax
 
+if [ "$?" != 0 ]
+then
+	cat <<EOF
+If you see errors it maight be necassary to install additional packages like
+autoconf >= 2.60
+automake >= 1.9
+libltdl (libltdl3-dev debian package)
+and all -devel packages mentioned in the README file
+EOF
+fi
