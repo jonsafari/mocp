ce0dbb4cea67bff07ab570c686550e8d5081ffa7
daper <daper@910807d9-36e0-0310-a014-e9ea483e2ba4>
2009-05-12 20:34:51 +0000

    Change minimum screen height to 7 lines. (Tero Marttila).
    git-svn-id: svn://svn.daper.net/moc/trunk@2151 910807d9-36e0-0310-a014-e9ea483e2ba4

--- interface_elements.c
+++ interface_elements.c
@@ -2356,7 +2356,7 @@ static void init_lines ()
 /* End the program if the terminal is too small. */
 static void check_term_size (struct main_win *mw, struct info_win *iw)
 {
-	mw->too_small = iw->too_small = COLS < 59 || LINES < 17;
+	mw->too_small = iw->too_small = COLS < 59 || LINES < 7;
 }
 
 /* Update the title with the current fill. */
