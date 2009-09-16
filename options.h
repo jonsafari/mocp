9934c8cadae57abbb063d48bf791ac12920f3cb7
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2009-09-16 06:41:06 +0000

    Add boolean and symbol typed options and accessors.  Code which accesses these new types as integers and strings will still continue to work (but the original contributors may wish to think about changing).
    Also provide access to an option's type.
    
    
    git-svn-id: svn://svn.daper.net/moc/trunk@2190 910807d9-36e0-0310-a014-e9ea483e2ba4

--- options.h
+++ options.h
@@ -1,8 +1,20 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
-char *options_get_str (const char *name);
+enum option_type
+{
+	OPTION_FREE = 0,
+	OPTION_INT  = 1,
+	OPTION_BOOL = 2,
+	OPTION_STR  = 4,
+	OPTION_SYMB = 8,
+	OPTION_ANY  = 255
+};
+
 int options_get_int (const char *name);
+_Bool options_get_bool (const char *name);
+char *options_get_str (const char *name);
+char *options_get_symb (const char *name);
 void option_set_int (const char *name, const int value);
 void option_set_str (const char *name, const char *value);
 void options_init ();
@@ -10,6 +22,9 @@ void options_parse (const char *config_file);
 void options_free ();
 void option_ignore_config (const char *name);
 int check_str_option (const char *name, const char *val);
+int check_symb_option (const char *name, const char *val);
 int check_int_option (const char *name, const int val);
+int check_bool_option (const char *name, const _Bool val);
+enum option_type options_get_type (const char *name);
 
 #endif
