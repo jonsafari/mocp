aa4aaa2564ebfea18c3136166102fe95909fea4a
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2010-11-13 08:49:32 +0000

    Have autogen.sh search for GNU libtoolize which some systems (e.g., MacOS) rename.
    Partially resolves: http://moc.daper.net/node/731
    Thanks to: Max Klinger
    
    
    git-svn-id: svn://svn.daper.net/moc/trunk@2255 910807d9-36e0-0310-a014-e9ea483e2ba4

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
+# Try to find where GNU libtool is hiding.
+LIBTOOL_AKA="glibtoolize"
+if test -z "$LIBTOOLIZE"; then
+	for LIBTOOL in $LIBTOOL_AKA libtoolize; do
+		LIBTOOLIZE=`which $LIBTOOL 2>/dev/null`
+		if test $? -eq 0; then
+			if $LIBTOOLIZE --version | grep -q "GNU libtool"; then
+				break
+			fi
+			LIBTOOLIZE=
+		fi
+	done
+	if test -z "$LIBTOOLIZE"; then
+		echo "No suitable libtoolize found (set LIBTOOLIZE if necessary)."
+		exit 1
+	fi
+fi
+
 set -x
 
 if test -f Makefile; then
 	make distclean >/dev/null 2>/dev/null
 fi
 
-libtoolize -c -f
+$LIBTOOLIZE -c -f
 aclocal -I m4 && \
 autoheader && \
 automake -W all -a && \
 autoconf -W syntax
 
-if [ "$?" != 0 ]
-then
+if test "$?" != 0; then
 	cat <<EOF
 If you see errors it might be necessary to install additional packages like
 autoconf >= 2.60
