74854d7d4f95f6827dc7edc076aec156245ac11a
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2012-09-17 21:50:31 +0000

    Fix memory corruption bug.
    There are no reported problems for which this bug is the obvious
    cause, but it's no doubt responsible for quite a bit of mischief.

--- trunk/protocol.c
+++ trunk/protocol.c
@@ -232,7 +232,7 @@ static void packet_buf_add_space (struct packet_buf *b, const size_t len)
 
 	if (b->allocated < b->len + len) {
 		b->allocated += len + 256; /* put some more space */
-		b->buf = (char *)xrealloc (b, b->allocated);
+		b->buf = (char *)xrealloc (b->buf, b->allocated);
 	}
 }
 
