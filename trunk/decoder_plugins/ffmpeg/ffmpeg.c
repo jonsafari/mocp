7696d2d1ce44aea847276eeedccab02aa5b3a905
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2012-09-17 20:47:42 +0000

    Disambiguate similarly prefixed extensions for display.
    Where an extension is longer than the desired three characters,
    use the first two and last characters which is more likely to give
    a unique string than just the first three.

--- trunk/decoder_plugins/ffmpeg/ffmpeg.c
+++ trunk/decoder_plugins/ffmpeg/ffmpeg.c
@@ -1183,6 +1183,8 @@ static void ffmpeg_get_name (const char *file, char buf[4])
 
 	ext = ext_pos (file);
 	strncpy (buf, ext, 3);
+	if (strlen (ext) > 3)
+		buf[2] = ext[strlen (ext) - 1];
 	for (ix = 0; ix < strlen (buf); ix += 1)
 		buf[ix] = toupper (buf[ix]);
 }
