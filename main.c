bfbd999b8fd11b7abffec01c3396e2f4e9140500
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2014-02-05 03:36:03 +0000

    Bump version numbers up to 2.5.0-beta2.
    git-svn-id: svn://svn.daper.net/moc/trunk@2604 910807d9-36e0-0310-a014-e9ea483e2ba4

--- main.c
+++ main.c
@@ -284,7 +284,7 @@ static void show_version ()
 	printf ("           Author : Damian Pietras\n");
 	printf ("         Homepage : %s\n", PACKAGE_URL);
 	printf ("           E-Mail : %s\n", PACKAGE_BUGREPORT);
-	printf ("        Copyright : (C) 2003-2012 Damian Pietras and others\n");
+	printf ("        Copyright : (C) 2003-2014 Damian Pietras and others\n");
 	printf ("          License : GNU General Public License, version 2 or later\n");
 	putchar ('\n');
 }
