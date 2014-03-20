91d5db88662122c9e04a9caf0ff3493717750285
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2014-03-20 00:18:27 +0000

    Align function declaration with function definition.
    git-svn-id: svn://svn.daper.net/moc/trunk@2620 910807d9-36e0-0310-a014-e9ea483e2ba4

--- equalizer.h
+++ equalizer.h
@@ -10,7 +10,7 @@ void equalizer_shutdown();
 void equalizer_process_buffer(char *buf, size_t size, const struct sound_params *sound_params);
 void equalizer_refresh();
 int equalizer_is_active();
-int equalizer_set_active();
+int equalizer_set_active(int active);
 char *equalizer_current_eqname();
 void equalizer_next();
 void equalizer_prev();
