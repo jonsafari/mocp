c7e22023a13d8f63b5cde231c0ec450e489e6ec0
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2014-05-03 03:04:00 +0000

    SndFile cannot play huge files in certain formats.
    This is a limitation of those formats rather than a bug in libsndfile.
    
    Possibly resolves: node/550 (Gigabyte-sized .WAVs?)

--- trunk/decoder_plugins/sndfile/sndfile.c
+++ trunk/decoder_plugins/sndfile/sndfile.c
@@ -13,6 +13,12 @@
 # include "config.h"
 #endif
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
 #include <string.h>
 #include <assert.h>
 #include <sndfile.h>
@@ -35,21 +41,77 @@ struct sndfile_data
 	SNDFILE *sndfile;
 	SF_INFO snd_info;
 	struct decoder_error error;
+	bool timing_broken;
 };
+
+/* Return true iff libsndfile's frame count is unknown or miscalculated. */
+static bool is_timing_broken (int fd, struct sndfile_data *data)
+{
+	int rc;
+	struct stat buf;
+	SF_INFO *info = &data->snd_info;
+
+	if (info->frames == SF_COUNT_MAX)
+			return true;
+
+	if (info->frames / info->samplerate > INT32_MAX)
+			return true;
+
+	/* The libsndfile code warns of miscalculation for huge files of
+	 * specific formats, but it's unclear if others are known to work
+	 * or the test is just omitted for them.  We'll assume they work
+	 * until it's shown otherwise. */
+	switch (info->format & SF_FORMAT_TYPEMASK) {
+	case SF_FORMAT_AIFF:
+	case SF_FORMAT_AU:
+	case SF_FORMAT_SVX:
+	case SF_FORMAT_WAV:
+		rc = fstat (fd, &buf);
+		if (rc == -1) {
+			logit ("Can't stat file: %s", strerror (errno));
+			/* We really need to return "unknown" here. */
+			return false;
+		}
+
+		if (buf.st_size > UINT32_MAX)
+			return true;
+	}
+
+	return false;
+}
 
 static void *sndfile_open (const char *file)
 {
+	int fd;
 	struct sndfile_data *data;
 
 	data = (struct sndfile_data *)xmalloc (sizeof(struct sndfile_data));
 
 	decoder_error_init (&data->error);
 	memset (&data->snd_info, 0, sizeof(data->snd_info));
+	data->timing_broken = false;
 
-	if (!(data->sndfile = sf_open(file, SFM_READ, &data->snd_info))) {
+	fd = open (file, O_RDONLY);
+	if (fd == -1) {
+		decoder_error (&data->error, ERROR_FATAL, 0,
+				"Can't open file: %s", strerror (errno));
+		return data;
+	}
+
+	/* sf_open_fd() close()s 'fd' on error and in sf_close(). */
+	data->sndfile = sf_open_fd (fd, SFM_READ, &data->snd_info, SF_TRUE);
+	if (!data->sndfile) {
 		/* FIXME: sf_strerror is not thread safe with NULL argument */
 		decoder_error (&data->error, ERROR_FATAL, 0,
 				"Can't open file: %s", sf_strerror(NULL));
+		return data;
+	}
+
+	/* If the timing is broken, sndfile only decodes up to the broken value. */
+	data->timing_broken = is_timing_broken (fd, data);
+	if (data->timing_broken) {
+		decoder_error (&data->error, ERROR_FATAL, 0,
+		               "File too large for audio format!");
 		return data;
 	}
 
@@ -76,11 +138,11 @@ static void sndfile_info (const char *file_name, struct file_tags *info,
 		const int tags_sel)
 {
 	if (tags_sel & TAGS_TIME) {
-		struct sndfile_data *data = sndfile_open (file_name);
+		struct sndfile_data *data;
 
-		if (data->sndfile && data->snd_info.frames < SF_COUNT_MAX)
+		data = sndfile_open (file_name);
+		if (data->sndfile && !data->timing_broken)
 			info->time = data->snd_info.frames / data->snd_info.samplerate;
-
 		sndfile_close (data);
 	}
 }
@@ -122,11 +184,14 @@ static int sndfile_get_bitrate (void *void_data ATTR_UNUSED)
 
 static int sndfile_get_duration (void *void_data)
 {
+	int result;
 	struct sndfile_data *data = (struct sndfile_data *)void_data;
 
-	return	data->snd_info.frames < SF_COUNT_MAX ?
-			data->snd_info.frames / data->snd_info.samplerate
-			: -1;
+	result = -1;
+	if (!data->timing_broken)
+		result = data->snd_info.frames / data->snd_info.samplerate;
+
+	return result;
 }
 
 static void sndfile_get_name (const char *file, char buf[4])
