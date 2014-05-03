7b21fa35d90bc8951ee50cbfa52a61ef7a2ad644
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2014-05-03 03:04:36 +0000

    Dead code removal.
    Amends: r2068
    
    git-svn-id: svn://svn.daper.net/moc/trunk@2634 910807d9-36e0-0310-a014-e9ea483e2ba4

--- playlist.h
+++ playlist.h
@@ -75,7 +75,6 @@ int plist_find_fname (struct plist *plist, const char *file);
 struct file_tags *tags_new ();
 void tags_clear (struct file_tags *tags);
 void tags_copy (struct file_tags *dst, const struct file_tags *src);
-size_t tags_mem (const struct file_tags *tags);
 struct file_tags *tags_dup (const struct file_tags *tags);
 void tags_free (struct file_tags *tags);
 char *build_title_with_format (const struct file_tags *tags, const char *fmt);
