03c778ed22789f16ac2f43b3dbaa2fc93f27e80e
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2014-09-10 02:36:15 +0000

    Manually add the compiler.h file which git-svn missed.
    Amends: r2693

---
+++ trunk/compiler.h
@@ -0,0 +1,49 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef HAVE__ATTRIBUTE__
+# define ATTR_UNUSED __attribute__((unused))
+#else
+# define ATTR_UNUSED
+#endif
+
+/* __FUNCTION__ is a gcc extension */
+#ifndef HAVE__FUNCTION__
+# define __FUNCTION__ "UNKNOWN_FUNC"
+#endif
+
+#ifndef GCC_VERSION
+#define GCC_VERSION (__GNUC__ * 10000 + \
+                     __GNUC_MINOR__ * 100 + \
+                     __GNUC_PATCHLEVEL__)
+#endif
+
+/* These macros allow us to use the appropriate method for manipulating
+ * GCC's diagnostic pragmas depending on the compiler's version. */
+#if GCC_VERSION >= 40200
+# define GCC_DIAG_STR(s) #s
+# define GCC_DIAG_JOINSTR(x,y) GCC_DIAG_STR(x ## y)
+# define GCC_DIAG_DO_PRAGMA(x) _Pragma (#x)
+# define GCC_DIAG_PRAGMA(x) GCC_DIAG_DO_PRAGMA(GCC diagnostic x)
+# if GCC_VERSION >= 40600
+#  define GCC_DIAG_OFF(x) GCC_DIAG_PRAGMA(push) \
+                          GCC_DIAG_PRAGMA(ignored GCC_DIAG_JOINSTR(-W,x))
+#  define GCC_DIAG_ON(x)  GCC_DIAG_PRAGMA(pop)
+# else
+#  define GCC_DIAG_OFF(x) GCC_DIAG_PRAGMA(ignored GCC_DIAG_JOINSTR(-W,x))
+#  define GCC_DIAG_ON(x)  GCC_DIAG_PRAGMA(warning GCC_DIAG_JOINSTR(-W,x))
+# endif
+#else
+# define GCC_DIAG_OFF(x)
+# define GCC_DIAG_ON(x)
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
