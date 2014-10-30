8ee7e39557c7c46d7bde70035509681757bbe3b6
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2014-10-30 02:41:28 +0000

    Localise output buffer implementation structures.
    git-svn-id: svn://svn.daper.net/moc/trunk@2751 910807d9-36e0-0310-a014-e9ea483e2ba4

--- out_buf.h
+++ out_buf.h
@@ -1,45 +1,18 @@
 #ifndef BUF_H
 #define BUF_H
 
-#include <pthread.h>
 #include "fifo_buf.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef void (out_buf_free_callback)();
+typedef void out_buf_free_callback ();
 
-struct out_buf
-{
-	struct fifo_buf *buf;
-	pthread_mutex_t	mutex;
-	pthread_t tid;	/* Thread id of the reading thread. */
+struct out_buf;
 
-	/* Signals. */
-	pthread_cond_t play_cond;	/* Something was written to the buffer. */
-	pthread_cond_t ready_cond;	/* There is some space in the buffer. */
-
-	/* Optional callback called when there is some free space in
-	 * the buffer. */
-	out_buf_free_callback *free_callback;
-
-	/* State flags of the buffer. */
-	int pause;
-	int exit;	/* Exit when the buffer is empty. */
-	int stop;	/* Don't play anything. */
-
-	int reset_dev;	/* Request to the reading thread to reset the audio
-			   device. */
-
-	float time;	/* Time of played sound. */
-	int hardware_buf_fill;	/* How the sound card buffer is filled. */
-
-	int read_thread_waiting; /* Is the read thread waiting for data? */
-};
-
-void out_buf_init (struct out_buf *buf, int size);
-void out_buf_destroy (struct out_buf *buf);
+struct out_buf *out_buf_new (int size);
+void out_buf_free (struct out_buf *buf);
 int out_buf_put (struct out_buf *buf, const char *data, int size);
 void out_buf_pause (struct out_buf *buf);
 void out_buf_unpause (struct out_buf *buf);
