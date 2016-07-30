f13be0284bb8f7ac4bb8f090a57283a643f0f753
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2016-07-30 03:17:33 +0000

    Fix: Unintentional disabling of MMAP support.
    The conversion of SIZE_MAX (unsigned) to off_t (signed) causes a
    comparison against -1, which is always false.
    
    Amends: r2594
    
    git-svn-id: svn://svn.daper.net/moc/trunk@2904 910807d9-36e0-0310-a014-e9ea483e2ba4

--- io.c
+++ io.c
@@ -75,7 +75,7 @@ static ssize_t io_read_mmap (struct io_stream *s, const int dont_move,
 		s->size = file_stat.st_size;
 		s->mem = NULL;
 
-		if (!RANGE(1, s->size, (off_t)SIZE_MAX)) {
+		if (s->size < 1 || (uint64_t)s->size > SIZE_MAX) {
 			logit ("File size unsuitable for mmap()");
 			return -1;
 		}
@@ -157,8 +157,6 @@ static ssize_t io_internal_read (struct io_stream *s, const int dont_move,
 #ifdef HAVE_MMAP
 static off_t io_seek_mmap (struct io_stream *s, const off_t where)
 {
-	assert (LIMIT(where, s->size));
-
 	return (s->mem_pos = where);
 }
 #endif
@@ -459,7 +457,8 @@ static void io_open_file (struct io_stream *s, const char *file)
 		s->size = file_stat.st_size;
 
 #ifdef HAVE_MMAP
-		if (options_get_bool ("UseMMap") && RANGE(1, s->size, (off_t)SIZE_MAX)) {
+		if (options_get_bool ("UseMMap") &&
+		    s->size > 0 && (uint64_t)s->size <= SIZE_MAX) {
 			s->mem = mmap (0, (size_t)s->size, PROT_READ, MAP_SHARED, s->fd, 0);
 			if (s->mem == MAP_FAILED) {
 				s->mem = NULL;
