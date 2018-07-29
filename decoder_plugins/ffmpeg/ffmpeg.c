b6029ca25e9bf605b9328589913b3843859a892c
jcf <jcf@910807d9-36e0-0310-a014-e9ea483e2ba4>
2018-07-29 21:48:37 +0000

    Fix: FFmpeg/LibAV seekable flag incorrectly tested.
    Ref: FFmpeg commit 83548fe8
    
    git-svn-id: svn://svn.daper.net/moc/trunk@2976 910807d9-36e0-0310-a014-e9ea483e2ba4

--- decoder_plugins/ffmpeg/ffmpeg.c
+++ decoder_plugins/ffmpeg/ffmpeg.c
@@ -508,7 +508,7 @@ static bool is_seek_broken (struct ffmpeg_data *data)
 #endif
 
 	/* How much do we trust this? */
-	if (!data->ic->pb->seekable) {
+	if (!(data->ic->pb->seekable & AVIO_SEEKABLE_NORMAL)) {
 		debug ("Seek broken by AVIOContext.seekable");
 		return true;
 	}
